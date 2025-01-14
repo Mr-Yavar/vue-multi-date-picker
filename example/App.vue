4d695fe9cd957c9323a79bd215a0423b4e612e83
Abbas Yavar <Mr_yavar@outlook.com>
2025-01-14 10:48:14 +0330

    ss

--- example/App.vue
+++ example/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Container from '@/container/Container.vue'
-import persian from 'react-date-object/calendars/persian'
-import persian_fa from 'react-date-object/locales/persian_fa'
+import PickerContainer from '@/container/PickerContainer.vue';
+import persian from 'react-date-object/calendars/persian';
+import persian_fa from 'react-date-object/locales/persian_fa'; /* PartiallyEnd: #3632/scriptSetup.vue */
 </script>
 
 <template>
     <div>
-        <Container :type="'TIME'" :subType="'TIME'" :date-separator="'/'" :calendar="persian" :locale="persian_fa" format="YYYY-MM-DD HH:mm:ss" />
+        <PickerContainer :type="'RANGE_DATE'" :subType="'DAY'" :date-separator="'/'" :calendar="persian" :locale="persian_fa" format="YYYY-MM-DD HH:mm:ss" />
     </div>
 </template>
